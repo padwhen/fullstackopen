2bebd469efa6f233286f82898097c35eb86eed6d
padwhen <phattrong.a@gmail.com>
2023-05-04 01:28:33 +0300

    3.2

--- part3/hello/index.js
+++ part3/hello/index.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
+app.use(requestLogger)
+app.use(unknownEndpoint)
 let notes = [
     {
       id: 1,
