753ea4dd4522db2ffe54d798bb2f9ebdcb39cc92
padwhen <123895854+padwhen@users.noreply.github.com>
2023-10-29 23:35:39 +0200

    3.1 3.2 3.3 3.4 3.5 3.6 [Updated]

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -29,9 +29,43 @@ let phonebook =
 app.get('/', (request, response) => {
   response.json("test")
 })
+
 app.get('/api/persons', (request, response) => {
     response.json(phonebook)
 })
+
+app.get('/info', (request, response) => {
+  const length = Math.max(...phonebook.map(p => p.id))
+  const now = new Date()
+  response.json(`Phonebook has info for ${length} people <br /> ${now}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = phonebook.find(person => person.id == id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  phonebook = phonebook.filter(person => person.id !== id)
+  response.status(204).json(error: 'name must be uin')
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body;
+  if (!person.name || !person.number) {
+    response.status(204).
+  }
+  const id = Math.floor(Math.random() * 1000000)
+  phonebook = phonebook.concat(person)
+  response.json(phonebook)
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
