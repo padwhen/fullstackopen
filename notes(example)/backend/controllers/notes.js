e9df0fb2a5adc9adc292a4475e70b6a0150a2d55
padwhen <123895854+padwhen@users.noreply.github.com>
2023-11-08 12:26:58 +0200

    before express-async-errors

--- notes(example)/backend/controllers/notes.js
+++ notes(example)/backend/controllers/notes.js
@@ -1,43 +1,58 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-    .then(note => { note ? response.json(note) : response.status(404).end() })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
     .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
     const body = request.body
+  
     const note = new Note({
-        content: body.content,
-        important: body.important || false
+      content: body.content,
+      important: body.important || false,
     })
-    note.save()
-    .then(savedNote => { response.json(savedNote )})
-    .catch(error => next(error))
-})
+    try {
+        const savedNote = await note.save()
+        response.status(201).json(savedNote)        
+    } catch (exception) {
+        next(exception)
+    }
+  })
 
 notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-    .then(() => { response.status(204).end() })
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
     .catch(error => next(error))
 })
 
 notesRouter.put('/:id', (request, response, next) => {
-    const body = request.body
-    const note = {
-        content: body.content,
-        important: body.important
-    }
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => { response.json(updatedNote)})
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
     .catch(error => next(error))
 })
 
